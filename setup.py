e0550bdad37dcb8ce77dded543f27b730764ac6e
Weyland <71197790+wey-chiang@users.noreply.github.com>
2022-05-06 12:49:21 -0400

    Added manifest.in and version bump

--- setup.py
+++ setup.py
@@ -21,14 +21,14 @@ with open('requirements.txt') as f:
 setup(
     name='panther_core',
     packages=['panther_core'],
-    version='0.0.1',
+    version='0.0.2',
     license='AGPL-3.0',
     description=
     'Panther command line interface for writing, testing, and packaging policies/rules.',
     author='Panther Labs Inc',
     author_email='pypi@runpanther.io',
     url='https://github.com/panther-labs/panther_core',
-    download_url = 'https://github.com/panther-labs/panther_core/archive/v0.0.1.tar.gz',
+    download_url = 'https://github.com/panther-labs/panther_core/archive/v0.0.2.tar.gz',
     keywords=['Security', 'CLI'],
     install_requires=install_requires,
     classifiers=[
